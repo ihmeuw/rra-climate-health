a8792b5ed77b2b30982ac9a75f2959a92c8693f5
James Collins <collijk@uw.edu>
2024-06-25 02:42:44 -0700

    Add some ld_library_path manipulation

--- src/spatial_temp_cgf/__init__.py
+++ src/spatial_temp_cgf/__init__.py
@@ -1,3 +1,12 @@
-from ctypes import cdll
-# Lmer needs to know where to find libstdc++.so.6
-cdll.LoadLibrary('/mnt/share/homes/victorvt/envs/cgf_temperature/lib/libstdc++.so.6')
+import os
+import sys
+from pathlib import Path
+# We're using an R library for modeling and R is so so dumb.  It doesn't know how to
+# find its own libraries. So we have to tell it where to look.
+os.environ['LD_LIBRARY_PATH'] = (
+    f"{os.environ['LD_LIBRARY_PATH']}:{Path(sys.executable).parent.parent / 'lib'}"
+)
+
+del os
+del sys
+del Path
