c8d319320bff6a0145397f53b50efd5be3cd6034
Victor M. Vilchis Tella <victorvt@uw.edu>
2024-08-12 09:45:38 -0700

    Adding more diagnostics plots

--- src/spatial_temp_cgf/inference/inference_diagnostics.py
+++ src/spatial_temp_cgf/inference/inference_diagnostics.py
@@ -6,6 +6,8 @@ import pandas as pd
 import seaborn as sns
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
 import numpy as np
 from io import BytesIO
 
@@ -123,7 +125,7 @@ def save_plot_to_bytes(fig=None):
     buf.seek(0)
     return buf
 
-def create_inference_diagnostics_report(output_path, measure, results_version):
+def create_inference_diagnostics_report(output_path, measure, results_version, model_version):
     cm_data = ClimateMalnutritionData(output_path / measure)
 
     output_path = cm_data.results / results_version / f"forecast_diag.pdf"
@@ -139,7 +141,203 @@ def create_inference_diagnostics_report(output_path, measure, results_version):
 
     cumulative_differences = get_cumulative_differences(forecast)
     elements.extend(create_table(cumulative_differences, "Cumulative Differences", as_integers=True))
-    elements.append(Image(save_plot_to_bytes(plot_forecast_prevalence(forecast, "Prevalence")), width=500, height=300))
+    elements.append(Image(save_plot_to_bytes(plot_forecast_prevalence(forecast, "Prevalence")), width=600, height=450))
+    elements.append(Image(save_plot_to_bytes(plot_gbd_comparison(measure, results_version, model_version, results_version)), width=600, height=450))
+
+    elements.append(Spacer(1, 30))
+    elements.append(Paragraph("Model Diagnostics", styles['Heading2']))
+    elements.append(Image(save_plot_to_bytes(plot_model_heatmaps(model_version, measure, measure)), width=600, height=200))
+    elements.extend(create_table(get_coefficients_table(cm_data, model_version).reset_index(), "Coefficients", as_integers=False))
+    elements.extend(create_table(get_re_table(cm_data, model_version).reset_index(), "Random Effects", as_integers=False))
 
     doc.build(elements)
 
+def plot_gbd_comparison(measure, version_label, model_version, results_version, scenarios = ['ssp119'], 
+    age_group_ids = [4,5], sex_ids = [1,2], year_ids = list(range(2020, 2023))):
+    from spatial_temp_cgf.data_prep.location_mapping import FHS_HIERARCHY_PATH
+    import pickle
+    ROOT = Path(DEFAULT_ROOT)
+    fhs_loc_meta = pd.read_parquet(FHS_HIERARCHY_PATH).sort_values('sort_order').reset_index(drop=True)
+    def load_model(root: Path, measure: str, version: str, age_group_id: int, sex_id: int):
+        with open(root / measure / 'models'/ version / f'{age_group_id}_{sex_id}.pkl', 'rb') as file:
+            model = pickle.load(file)
+        return model
+
+    def load_random_effects(root: Path, measure: str, version: str,
+                            version_label: str, age_group_id: int, sex_id: int, fhs_loc_meta: pd.DataFrame):
+        data = load_model(root, measure, version, age_group_id, sex_id).ranef
+        data['version_label'] = version_label
+        data['age_group_id'] = age_group_id
+        data['sex_id'] = sex_id
+        data['measure'] = measure
+        fhs_loc_meta['ihme_loc_id'] = fhs_loc_meta['ihme_loc_id'].str[:3]
+        data = data.join(fhs_loc_meta.set_index('ihme_loc_id').loc[:, 'location_id'])
+
+        return data.set_index(['version_label', 'measure', 'location_id', 'age_group_id', 'sex_id']).loc[:, 'X.Intercept.']
+
+
+    def load_prediction(year_id: int, root: Path, measure: str, version: str, version_label: str, scenario: str):
+        data = pd.read_parquet(root / measure / 'results' / version / f'{year_id}_{scenario}.parquet')
+        data['year_id'] = year_id
+        data['version_label'] = version_label
+        data['measure'] = measure
+
+        return data.set_index(['version_label', 'measure', 'location_id', 'year_id', 'age_group_id', 'sex_id']).loc[:, 'value'].sort_index()
+
+    random_effects = []
+    predictions = []
+    for age_group_id in age_group_ids:
+        for sex_id in sex_ids:
+            random_effects.append(
+                load_random_effects(ROOT, measure, model_version,
+                                    version_label, age_group_id, sex_id, fhs_loc_meta)
+            )
+    for year_id in year_ids:
+        for scenario in scenarios:
+            predictions.append(load_prediction(year_id, ROOT, measure, results_version, version_label, scenario))
+    predictions = pd.concat(predictions).sort_index().rename('pred')
+    random_effects = pd.concat(random_effects).sort_index().rename('ranef')
+
+    # age_metadata = db_queries.get_age_metadata(release_id=9)
+    age_metadata = pd.read_parquet(ROOT / 'input' / 'gbd_prevalence' / 'age_metadata.parquet')
+    age_metadata = age_metadata.loc[age_metadata['age_group_days_start'] >= 28]
+    age_metadata = age_metadata.loc[age_metadata['age_group_years_end'] <= 5]
+
+    population = pd.read_parquet(ROOT / 'input' / 'gbd_prevalence' / 'population.parquet')
+
+    gbd = pd.read_parquet(ROOT / 'input' / 'gbd_prevalence' / f'{measure}.parquet')
+    gbd['measure'] = measure
+    gbd = gbd.groupby(['measure', 'location_id', 'year_id', 'age_group_id', 'sex_id'], as_index=False)['mean'].sum()
+
+    gbd = gbd.merge(population)
+    gbd = gbd.merge(age_metadata.loc[:, ['age_group_id', 'age_group_years_start', 'age_group_years_end']])
+    gbd.loc[gbd['age_group_years_end'] <= 1, 'age_group_id'] = 4
+    gbd.loc[gbd['age_group_years_start'] >= 1, 'age_group_id'] = 5
+    gbd['gbd'] = gbd['mean'] * gbd['population']
+    gbd = gbd.groupby(['measure', 'location_id', 'year_id', 'age_group_id', 'sex_id'])['gbd', 'population'].sum()
+    gbd['gbd'] /= gbd['population']
+
+    plot_data = predictions.to_frame().join(gbd).dropna().join(random_effects, how='left').reorder_levels(predictions.index.names)
+    plot_data['has_raneff'] = plot_data['ranef'].notnull()
+    #plot_data = plot_data.loc[:, :, :, :, [4], [1]]
+    plot_data['pred'] *= plot_data['population']
+    plot_data['gbd'] *= plot_data['population']
+    plot_data = plot_data.groupby(['version_label', 'measure', 'has_raneff', 'location_id', 'year_id'])['pred', 'gbd', 'population'].sum()
+    plot_data['pred'] /= plot_data['population']
+    plot_data['gbd'] /= plot_data['population']
+    plot_data = plot_data.drop('population', axis=1)
+    plot_data.describe()
+
+    sns.set_style('whitegrid')
+
+    marker = 'o'
+    # Only one figure, no subplots
+    fig, ax = plt.subplots(figsize=(7, 5))
+    
+    label = f"{version_label}"
+    sublabel = f""
+    
+    for re, color in [(True, 'mediumseagreen'), (False, 'mediumorchid')]:
+        rmse = np.round(
+            (
+                (
+                    plot_data.loc[version_label, measure, re, :, :].loc[:, 'gbd'] - plot_data.loc[version_label, measure, re, :, :].loc[:, 'pred']
+                ) ** 2
+            ).mean() ** 0.5,
+            4
+        )
+        if re:
+            legend_label = f'{measure}, locs in model (RMSE: {rmse})'
+        else:
+            legend_label = f'{measure}, locs not in model (RMSE: {rmse})'
+        ax.scatter(
+            plot_data.loc[version_label, measure, re, :, :].loc[:, 'gbd'],
+            plot_data.loc[version_label, measure, re, :, :].loc[:, 'pred'],
+            color=color,
+            s=100,
+            alpha=0.1,
+            marker=marker,
+        )
+        ax.scatter(
+            np.nan,
+            np.nan,
+            color=color,
+            label=legend_label,
+            s=100,
+            alpha=1.,
+            marker=marker,
+        )
+    ax.plot((0, 1), (0, 1), color='red')
+    ax.set_ylabel('Prediction')
+    ax.set_xlabel('GBD 2021')
+    ax.set_ylim(0, 0.6)
+    ax.set_xlim(0, 0.6)
+    ax.set_title(label)
+    ax.legend()
+    fig.tight_layout()
+    return fig
+
+def get_coefficients_table(cm_data, model_version):
+    models = cm_data.load_model_family(model_version)
+    return pd.concat([model['model'].coefs['Estimate'].rename(f"{model['age_group_id']} {model['sex_id']}") for model in models], axis=1)
+
+def get_re_table(cm_data, model_version):
+    models = cm_data.load_model_family(model_version)
+    return pd.concat([model['model'].ranef.rename(columns = {'X.Intercept.': f"{model['age_group_id']} {model['sex_id']}"}) for model in models], axis=1)
+
+def plot_model_heatmaps(model_version, measure, fig_title):
+    cm_data = ClimateMalnutritionData(Path(DEFAULT_ROOT) / measure)
+    models = cm_data.load_model_family(model_version)
+    df = pd.concat([model['model'].raw_data for model in models]).reset_index(drop=True)
+    df['pred'] = pd.concat([pd.Series(model['model'].predict(model['model'].design_matrix, use_rfx=False, verify_predictions=False)) for model in models]).reset_index(drop=True)
+    df['fits'] = pd.concat([model['model'].data for model in models]).reset_index(drop=True).fits
+    df['over_30'], o30_bins = pd.cut(df.days_over_30C, [0, 1, 2, 7, 15, 30, 60, 90, 180, 367], right=False, retbins=True)
+    df['ldi'], ldi_bins  = pd.qcut(df.ldi_pc_pd, 10, retbins=True)
+#    df['pred'] = m.predict(m.design_matrix, use_rfx=False, verify_predictions=False)
+    x_ticks = range(len(o30_bins) )
+    x_labs =  [f"{x:.1f}" for x in o30_bins] #+ [f"{df[temp_value_col].max():.1f}"]
+    #x_labs = [x for x in x_labs if x != 'nan']
+
+    y_ticks = range(len(ldi_bins) )
+    y_labs =  [f"{x:.1f}" for x in ldi_bins]
+
+    vmin = 0
+    vmax = 0.6 if measure == 'stunting' else 0.25
+    colorbin_interval = 0.05
+    boundaries = np.arange(vmin, vmax + colorbin_interval, colorbin_interval)
+    cmap = plt.get_cmap('RdYlBu_r', len(boundaries) - 1)
+    norm = mcolors.BoundaryNorm(boundaries, cmap.N, clip=True)
+
+    fig, axes = plt.subplots(figsize=(24, 8), ncols=3)
+    
+    sns.heatmap(df.groupby(['ldi', 'over_30'])[measure].mean().unstack(), ax=axes[0], 
+            annot=True, fmt=".2f", annot_kws={"size": 10, "weight":'regular'}, 
+            cmap=cmap, norm=norm, vmin=vmin, vmax=vmax, cbar_kws={"ticks": boundaries})
+    axes[0].set_title('Data')
+
+    sns.heatmap(df.groupby(['ldi', 'over_30']).fits.mean().unstack(), ax=axes[1],
+            annot=True, fmt=".2f", #annot_kws={"size": 14, "weight":'regular'}, 
+            cmap=cmap, norm=norm, vmin=vmin, vmax=vmax, cbar_kws={"ticks": boundaries})
+    axes[1].set_title('With location random effects')
+
+    sns.heatmap(df.groupby(['ldi', 'over_30']).pred.mean().unstack(),  ax=axes[2],
+            annot=True, fmt=".2f", annot_kws={"size": 10, "weight":'regular'}, #cmap='RdYlBu_r',
+            cmap=cmap, norm=norm, vmin=vmin, vmax=vmax, cbar_kws={"ticks": boundaries})
+    axes[2].set_title('Without location random effects')
+    for ax in axes:
+        ax.set_xticks(x_ticks)
+        ax.set_xticklabels(x_labs, rotation=45,  fontsize=12)
+        ax.set_yticks(y_ticks)
+        ax.set_yticklabels(y_labs,  fontsize=12)
+        ax.set_xlabel('Days over 30C', fontsize=12)
+        ax.set_ylabel('Income', fontsize=12)
+        ax.collections[0].colorbar.set_label(f'{measure.capitalize()} Prevalence', size=12)
+        #ax.collections[0].colorbar.ax.tick_params(labelsize=14)
+        #ax.tick_params(axis='both', which='major', labelsize=14)
+
+    #fig.suptitle(fig_title, fontsize=18)
+    fig.tight_layout()
+    #plt.show()
+    return fig
+    #return df
+    
