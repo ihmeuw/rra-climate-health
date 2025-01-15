a436a7312f0897cc2146cdb05e48468539ce7bf4
kristinhong1 <165940202+kristinhong1@users.noreply.github.com>
2025-01-15 15:52:44 -0800

    Minor edits to merge keys, renaming hhweight var

--- src/rra_climate_health/data_prep/extraction_scripts/merge_cgf_wealth.R
+++ src/rra_climate_health/data_prep/extraction_scripts/merge_cgf_wealth.R
@@ -22,9 +22,13 @@ convert_keys_to_char <- function(dt, keys) {
 cgf <- convert_keys_to_char(cgf, merge_keys)
 wealth <- convert_keys_to_char(wealth, merge_keys)
 
-## Merging and exporting
+## Merging
 tmp <- merge(cgf, wealth[, !c('file_path','survey_module')], 
-             by.x = c('hh_id','nid','strata','psu','pweight','ihme_loc_id','year_start','year_end','geospatial_id'), 
-             by.y = c('hh_id','nid','strata','psu','weight','iso3','year_start','year_end','geospatial_id'), 
+             by.x = c('hh_id','nid','strata','psu','ihme_loc_id','year_start','year_end','geospatial_id','survey_name','int_year'), 
+             by.y = c('hh_id','nid','strata','psu','iso3','year_start','year_end','geospatial_id','source','year'), 
              all.x = T)
+
+## Renaming weight to hhweight for clarity
+tmp <- setnames(tmp, 'weight', 'hhweight')
+
 write.csv(tmp, '/mnt/team/rapidresponse/pub/population/modeling/climate_malnutrition/input/data_01_06_2025/2_initial_processing/merged_cgf_wealth.csv')
