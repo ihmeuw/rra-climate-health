a55a9afe366e506ec25f0419553aef52b694d19d
Elye Bliss <elyeb@long-slurm-sarchive-p0011.cluster.ihme.washington.edu>
2026-01-05 13:06:16 -0800

    graphing grouped residuals

--- notebooks/neonatal/results_plots/covariates_vs_residuals_no_smooth.R
+++ notebooks/neonatal/results_plots/covariates_vs_residuals_no_smooth.R
@@ -40,7 +40,7 @@ options(scipen = 999) # turn off scientific notation
 #==============================================================================
 
 ## set parameters
-summary_file <- paste0("nnm_1_mo_q95_no_smooth_summary")
+summary_file <- paste0("nnm_1_mo_q95_summary")
 
 results_dir <- "/mnt/team/rapidresponse/pub/population/modeling/climate_malnutrition/neonatal_mortality/results/2025_12_16.01/"
 neo_version <- "/mnt/team/rapidresponse/pub/population/modeling/climate_malnutrition/neonatal_mortality/training_data/2025_12_16.01/neonatal_data.parquet"
@@ -113,38 +113,135 @@ residuals <- model_data$child_mortality - predicted_probs
 model_data[,residuals := residuals]
 
 range(model_data$residuals)
+mean(model_data$residuals) #0.00000000006939527
 
+# Grouping variable(s) - replace "group_var" with your desired grouping column(s)
+group_vars <- c("ihme_loc_id", "birth_year")  # Example grouping variables
+
+# Aggregate observed and predicted values by group
+grouped_residuals <- model_data[, .(
+  observed = mean(child_mortality, na.rm = TRUE),
+  predicted = mean(predicted_probs, na.rm = TRUE),
+  consumption_pd = mean(consumption_pd,na.rm=TRUE),
+  q95_prev_0_mo = mean(q95_prev_0_mo,na.rm=TRUE),
+  total_precipitation_prev_0_mo = mean(total_precipitation_prev_0_mo,na.rm=TRUE),
+  sex_id = mean(sex_id,na.rm=TRUE)
+), by = group_vars]
+
+# Calculate residuals at the group level
+grouped_residuals[, residuals := observed - predicted]
+
+# View the range of grouped residuals
+range(grouped_residuals$residuals)
+range(grouped_residuals$observed)
+mean(grouped_residuals$residuals) #0.007781135
 
 #==============================================================================
 # SECTION 3: PLOT COVARIATES AGAINST MODEL RESIDUALS
 #==============================================================================
 
-outfile <- gsub("summary","residuals_vs_covariates.pdf",summary_file)
 
 # Define the covariates to plot
-plot_covs <- c("consumption_pd",
-               "q95_prev_0_mo",
-               "total_precipitation_prev_0_mo",
-               "sex_id")
+# plot_covs <- c("consumption_pd",
+#                "q95_prev_0_mo",
+#                "total_precipitation_prev_0_mo",
+#                "sex_id")
+# 
+# # Save "consumption_pd" plot as PNG
+# png(paste0(plot_dir, gsub("summary", "residuals_vs_consumption_pd", summary_file), ".png"), width = 800, height = 600, res = 150)
+# ggplot(model_data, aes(x = consumption_pd, y = residuals)) +
+#   geom_point(alpha = 0.5) +
+#   labs(title = "Residuals vs consumption_pd",
+#        x = "consumption_pd",
+#        y = "Residuals") +
+#   theme_minimal()
+# dev.off()
+# 
+# # Save "q95_prev_0_mo" plot as PNG
+# png(paste0(plot_dir, gsub("summary", "residuals_vs_q95_prev_0_mo", summary_file), ".png"), width = 800, height = 600, res = 150)
+# ggplot(model_data, aes(x = q95_prev_0_mo, y = residuals)) +
+#   geom_point(alpha = 0.5) +
+#   labs(title = "Residuals vs q95_prev_0_mo",
+#        x = "q95_prev_0_mo",
+#        y = "Residuals") +
+#   theme_minimal()
+# dev.off()
+# 
+# # Save "total_precipitation_prev_0_mo" plot as PNG
+# png(paste0(plot_dir, gsub("summary", "residuals_vs_total_precipitation_prev_0_mo", summary_file), ".png"), width = 800, height = 600, res = 150)
+# ggplot(model_data, aes(x = total_precipitation_prev_0_mo, y = residuals)) +
+#   geom_point(alpha = 0.5) +
+#   labs(title = "Residuals vs total_precipitation_prev_0_mo",
+#        x = "total_precipitation_prev_0_mo",
+#        y = "Residuals") +
+#   theme_minimal()
+# dev.off()
+# 
+# # Save "sex_id" plot as PNG
+# png(paste0(plot_dir, gsub("summary", "residuals_vs_sex_id", summary_file), ".png"), width = 800, height = 600, res = 150)
+# ggplot(model_data, aes(x = sex_id, y = residuals)) +
+#   geom_point(alpha = 0.5) +
+#   labs(title = "Residuals vs sex_id",
+#        x = "sex_id",
+#        y = "Residuals") +
+#   theme_minimal()
+# dev.off()
 
-# Create a function to generate and save plots
-generate_and_save_plot <- function(covariate, data, plot_dir, outfile_prefix) {
-  p <- ggplot(data, aes_string(x = covariate, y = "residuals")) +
-    geom_point(alpha = 0.5) +
-    labs(title = paste("Residuals vs", covariate),
-         x = covariate,
-         y = "Residuals") +
-    theme_minimal()
-  
-  # Save the plot as a PDF
-  pdf_filename <- paste0(plot_dir, outfile_prefix, "_", covariate, ".pdf")
-  pdf(pdf_filename, width = 8, height = 6)
-  print(p)
-  dev.off()
-}
 
-# Generate and save each plot
-outfile_prefix <- gsub("summary", "residuals_vs_covariates", summary_file)
-for (covariate in plot_covs) {
-  generate_and_save_plot(covariate, model_data, plot_dir, outfile_prefix)
-}
+# Save "consumption_pd" plot as PNG
+png(paste0(plot_dir, gsub("summary", "grouped_residuals_vs_consumption_pd", summary_file), ".png"), width = 800, height = 600, res = 150)
+ggplot(grouped_residuals, aes(x = consumption_pd, y = residuals)) +
+  geom_point(alpha = 0.2, color = "blue") +
+  geom_hline(yintercept = 0, color = "black") +
+  labs(title = "Residuals vs consumption_pd,\ngrouped by country and birth year",
+       x = "consumption_pd",
+       y = "Residuals") +
+  theme_minimal()
+dev.off()
+
+# Save "q95_prev_0_mo" plot as PNG
+png(paste0(plot_dir, gsub("summary", "grouped_residuals_vs_q95_prev_0_mo", summary_file), ".png"), width = 800, height = 600, res = 150)
+ggplot(grouped_residuals, aes(x = q95_prev_0_mo, y = residuals)) +
+  geom_point(alpha = 0.2, color = "blue") +
+  geom_hline(yintercept = 0, color = "black") +
+  labs(title = "Residuals vs q95_prev_0_mo,\ngrouped by country and birth year",
+       x = "q95_prev_0_mo",
+       y = "Residuals") +
+  theme_minimal()
+dev.off()
+
+# Save "total_precipitation_prev_0_mo" plot as PNG
+png(paste0(plot_dir, gsub("summary", "grouped_residuals_vs_total_precipitation_prev_0_mo", summary_file), ".png"), width = 800, height = 600, res = 150)
+ggplot(grouped_residuals, aes(x = total_precipitation_prev_0_mo, y = residuals)) +
+  geom_point(alpha = 0.2, color = "blue") +
+  geom_hline(yintercept = 0, color = "black") +
+  labs(title = "Residuals vs total_precipitation_prev_0_mo,\ngrouped by country and birth year",
+       x = "total_precipitation_prev_0_mo",
+       y = "Residuals") +
+  theme_minimal()
+dev.off()
+
+# Save "sex_id" plot as PNG
+png(paste0(plot_dir, gsub("summary", "grouped_residuals_vs_sex_id", summary_file), ".png"), width = 800, height = 600, res = 150)
+ggplot(grouped_residuals, aes(x = sex_id, y = residuals)) +
+  geom_point(alpha = 0.2, color = "blue") +
+  geom_hline(yintercept = 0, color = "black") +
+  labs(title = "Residuals vs sex_id,\ngrouped by country and birth year",
+       x = "sex_id",
+       y = "Residuals") +
+  theme_minimal()
+dev.off()
+
+# Save "birth_year" plot as PNG
+png(paste0(plot_dir, gsub("summary", "grouped_residuals_vs_birth_year", summary_file), ".png"), width = 800, height = 600, res = 150)
+ggplot(grouped_residuals, aes(x = birth_year, y = residuals)) +
+  geom_point(alpha = 0.2, color = "blue") +
+  geom_hline(yintercept = 0, color = "black") +
+  labs(title = "Residuals vs birth_year,\ngrouped by country and birth year",
+       x = "birth_year",
+       y = "Residuals") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 5)  # Rotate x-axis labels and make font smaller
+  )
+dev.off()
